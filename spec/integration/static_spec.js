d0711075578f102e917149afcc65f0dbeca1a4b7
Melvin J <ibexwellness@gmail.com>
2018-12-25 13:02:34 -0500

    Marco Polo assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,4 +12,14 @@ describe("routes : static", () => {
     });
 
   });
+  describe("GET /marco", ()=> {
+  	const url = base + "marco";
+  	it("should return body containing string \"polo\"", ( done )=> {
+  		request.get(url, (err, res, body)=>{
+  			expect (res.statusCode).toBe( 200 );
+  			expect(body.toLowerCase()).toContain( "polo" );
+  			done();
+  		});
+  	});
+  });
 });

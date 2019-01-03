bc19c23e25e5041764999facc2a867b2e5ebef26
Melvin J <ibexwellness@gmail.com>
2019-01-03 12:14:18 -0500

    working commit

--- src/db/models/banner.js
+++ src/db/models/banner.js
@@ -4,20 +4,20 @@ module.exports = (sequelize, DataTypes) => {
     source: DataTypes.STRING,
     description: DataTypes.STRING,
     topicId: {
-    	type: DataTypes.INTEGER,
-    	onDelete: "CASCADE",
-    	references: {
-    		model: "Topics",
-    		key: "id",
-    		as: "topicId",
-    	}
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topics",
+        key: "id",
+        as: "topicId",
+      }
     }
   }, {});
   Banner.associate = function(models) {
     // associations can be defined here
     Banner.belongsTo(models.Topic, {
-    	foreignKey: "topicId",
-    	onDelete: "CASCADE",
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
     });
   };
   return Banner;

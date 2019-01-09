d9be4d37a43842454a3209c5c13286fbb2c25b45
Melvin J <ibexwellness@gmail.com>
2019-01-09 11:48:58 -0500

    new, create section done.

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -14,7 +14,7 @@ describe("routes : posts", () => {
 
     sequelize.sync({force: true}).then((res) => {
 
-//#1
+
       Topic.create({
         title: "Winter Games",
         description: "Post your Winter Games stories."
@@ -39,5 +39,48 @@ describe("routes : posts", () => {
     });
 
   });
+
+  describe("GET /topics/:topicId/posts/new", () => {
+
+    it("should render a new post form", (done) => {
+      request.get(`${base}/${Topic.id}/posts/new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Post");
+        done();
+      });
+    });
+
+  });
+
+  describe("POST /topics/:topicId/posts/create", () => {
+
+   it("should create a new post and redirect", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "Watching snow melt",
+          body: "Without a doubt my favoriting things to do besides watching paint dry!"
+        }
+      };
+      request.post(options,
+        (err, res, body) => {
+
+          Post.findOne({where: {title: "Watching snow melt"}})
+          .then((post) => {
+            expect(post).not.toBeNull();
+            expect(post.title).toBe("Watching snow melt");
+            expect(post.body).toBe("Without a doubt my favoriting things to do besides watching paint dry!");
+            expect(post.topicId).not.toBeNull();
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
+ });
 
 });
--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -2,5 +2,13 @@ const Post = require("./models").Post;
 const Topic = require("./models").Topic;
 
 module.exports = {
-	
+    addPost(newPost, callback){
+        return Post.create(newPost)
+        .then((post) => {
+          callback(null, post);
+        })
+        .catch((err) => {
+          callback(err);
+        })
+    },
 }

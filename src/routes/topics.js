0bad5b8449769d3cbca219cfdb59bd565abf0cf9
Melvin J <ibexwellness@gmail.com>
2019-01-23 16:48:59 -0500

    fixing merge issue

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -6,9 +6,11 @@ const validation = require("./validation");
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
 router.post("/topics/create", validation.validateTopics, topicController.create);
+router.get("/topics/:id/edit", topicController.edit);
 router.get("/topics/:id", topicController.show);
 router.post("/topics/:id/destroy", topicController.destroy);
-router.get("/topics/:id/edit", topicController.edit);
-router.post("/topics/:id/update", validation.validateTopics, topicController.update);
+router.post("/topics/:id/update", topicController.update);
+
+
 
 module.exports = router;

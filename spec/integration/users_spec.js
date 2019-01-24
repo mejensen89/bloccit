2eb7777e4ad522966dcc867db114113b723ce4c2
Melvin J <ibexwellness@gmail.com>
2019-01-24 17:50:22 -0500

    WIP 36s0f

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -15,6 +15,7 @@ describe("routes : users", () => {
       console.log(err);
       done();
     });
+
   });
 
   describe("GET /users/sign_up", () => {
@@ -25,6 +26,61 @@ describe("routes : users", () => {
         expect(body).toContain("Sign up");
         done();
       });
+    });
+
+  });
+
+  describe("POST /users", () => {
+
+    it("should create a new user with valid values and redirect", (done) => {
+
+      const options = {
+        url: base,
+        form: {
+          email: "user@example.com",
+          password: "123456789"
+        }
+      }
+
+      request.post(options,
+        (err, res, body) => {
+
+          User.findOne({where: {email: "user@example.com"}})
+          .then((user) => {
+            expect(user).not.toBeNull();
+            expect(user.email).toBe("user@example.com");
+            expect(user.id).toBe(1);
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
+    it("should not create a new user with invalid attributes and redirect", (done) => {
+      request.post(
+        {
+          url: base,
+          form: {
+            email: "no",
+            password: "123456789"
+          }
+        },
+        (err, res, body) => {
+          User.findOne({where: {email: "no"}})
+          .then((user) => {
+            expect(user).toBeNull();
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
     });
 
   });
